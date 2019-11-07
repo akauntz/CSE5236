00febe4242aab2acdf725caa5908341df670d70d
Mike Nose <nose.6@osu.edu>
2019-11-06 19:27:49 -0500

    Attempt at GPS

--- CSE_APP/app/src/main/java/com/example/mike9/cse_app/ui/main/HomeFragment.java
+++ CSE_APP/app/src/main/java/com/example/mike9/cse_app/ui/main/HomeFragment.java
@@ -1,14 +1,18 @@
 package com.example.mike9.cse_app.ui.main;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,8 +25,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.mike9.cse_app.GetLocation;
 import com.example.mike9.cse_app.MainActivity;
 import com.example.mike9.cse_app.MatchesActivity;
 import com.example.mike9.cse_app.R;
@@ -41,12 +48,18 @@ import java.util.Locale;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
+import static android.content.Context.LOCATION_SERVICE;
 
-public class HomeFragment extends Fragment implements View.OnClickListener  {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private String email;
     private EditText updatePass;
     TextView locationText;
+    GetLocation locationListener;
+    Criteria criteria;
+    LocationManager locationManager;
+    Looper looper;
+
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -56,26 +69,46 @@ public class HomeFragment extends Fragment implements View.OnClickListener  {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState){
+                             @Nullable Bundle savedInstanceState) {
         //TODO: fix the screen + check the default preferences for the user/move those to separate screen
-        View v = inflater.inflate(R.layout.home_fragment,container,false);
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
         Button updatePasswordButton = v.findViewById(R.id.updatePass_button);
         updatePasswordButton.setOnClickListener(this);
-        Button deleteAccountButton= v.findViewById(R.id.deleteAcct_button);
+        Button deleteAccountButton = v.findViewById(R.id.deleteAcct_button);
         deleteAccountButton.setOnClickListener(this);
         Button signOutButton = v.findViewById(R.id.signOut_button);
         signOutButton.setOnClickListener(this);
         Button getMatchesButton = v.findViewById(R.id.check_matches_button);
         getMatchesButton.setOnClickListener(this);
+        Button getLocationButton = v.findViewById(R.id.location_button);
+        getLocationButton.setOnClickListener(this);
+        locationText = v.findViewById(R.id.locationText);
         email = getArguments().getString("EMAIL");
         updatePass = v.findViewById(R.id.updatePass_text);
+        locationListener = new GetLocation();
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+        //locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        looper = null;
         return v;
     }
 
+   /* @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }*/
+
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         Activity activity = getActivity();
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.updatePass_button:
 
                 String newPass = updatePass.getText().toString();
@@ -115,13 +148,95 @@ public class HomeFragment extends Fragment implements View.OnClickListener  {
                 break;
 
             case R.id.location_button:
-                
+                Log.d("test", "here");
+
+                Log.d("test", "there");
+                if(locationAllowed()){
+                    Log.d("GPS", "Allowed");
+                    //LocationManager myLocation = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    //locationListener.getLocation(criteria, looper, getActivity());
+                    //locationText.setText(getCityLocation());
+                    setUpLocationListener();
+                } else {
+                    Log.d("GPS", "Denied");
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+                //gets here immediately when askign for gps
+                Log.d("GPS", "here");
 
 
-
-                locationText.setText("location2");
+                //locationText.setText("location2");
                 break;
 
+        }
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("GPS", "accepted");
+                } else {
+                    Log.d("GPS", "denied");
+                }
+                return;
+            }
+        }
+    }
+
+    public boolean locationAllowed(){
+        return (getActivity().checkCallingOrSelfPermission("android.permission.ACCESS_COARSE_LOCATION") == PackageManager.PERMISSION_GRANTED && getActivity().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public String getCityLocation(){
+        LocationManager locationManager = (LocationManager) locationListener.getSystemService(LOCATION_SERVICE);
+        if(locationAllowed()){
+            return "city";
+        }
+        return "city";
+    }
+
+    public void setUpLocationListener(){
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        /*if(locationAllowed()){
+            return;
+        }*/
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria,true);
+        if(ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Log.d("GPS", "Name GPS_PRovider:" + LocationManager.GPS_PROVIDER + "isEnabled? " +locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
+            if(location == null){
+                Log.d("GPS","No Location Found");
+            }
+            if (location != null){
+                Log.d("GPS","Location found" + location);
+            }
+        }
+
+    }
+    public void onLocationChanged(Location location){
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+        double lat = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        try{
+            Log.d("GPS", "Latitude, Longitude"+lat+" "+ longitude);
+            addresses = geocoder.getFromLocation(lat, longitude,1);
+            if (addresses != null && addresses.size() > 0){
+                String city = addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+
+                Log.d("GPS", "City: " + city);
+                Log.d("GPS", "State: " + state);
+            }
+        } catch (IOException e){
+            Log.d("errrrror",e.toString());
+            e.printStackTrace();
         }
     }
 }
