d83efc59f8f167240bb2852250f2facadbbbf20c
gnnnonlyone <zhou.2718@buckeyemail.osu.edu>
2019-11-19 22:41:57 -0500

    Localization

--- CSE_APP/app/src/main/java/com/example/mike9/cse_app/ui/main/MainFragment.java
+++ CSE_APP/app/src/main/java/com/example/mike9/cse_app/ui/main/MainFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,6 +53,7 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_fragment, container, false);
+
         Button matchesButton = v.findViewById(R.id.matches_button);
         matchesButton.setOnClickListener((View.OnClickListener) this);
         Button signUpButton = v.findViewById(R.id.signUp_button);
